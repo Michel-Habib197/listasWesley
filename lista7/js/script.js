c304977eb1bade9fffff4791d61d406019cdbff5
Michel-Habib197 <126814378+Michel-Habib197@users.noreply.github.com>
2025-09-11 20:24:55 +0000

    Update

--- lista7/js/script.js
+++ lista7/js/script.js
@@ -1,22 +1,35 @@
-function calcularTroco(){
-     const notas = [50, 20, 10, 5, 2, 1];
+function calcularTroco() {
+  const notas = [50, 20, 10, 5, 2, 1];
 
-    const valorPago = parseFloat(document.getElementById("valorPago").value);
-    const valorCompra = parseFloat(document.getElementById("valorCompra").value);
+  const valorPago = parseFloat(document.getElementById("valorPago").value);
+  const valorCompra = parseFloat(document.getElementById("valorCompra").value);
 
-    if(valorPago < valorCompra){
-        //a máquina deve apresentar uma mensagem, informando que a quantia paga é insuficiente para realizar a compra.
-        console.log("A quantia paga é insuficiente para realizar a compra!");
-        return;
-    }
+  if (valorPago < valorCompra) {
+    //a máquina deve apresentar uma mensagem, informando que a quantia paga é insuficiente para realizar a compra.
+    console.log("A quantia paga é insuficiente para realizar a compra!");
+    return;
+  }
 
-      let troco = valorPago - valorCompra;
-      resultado += `<p><strong>Troco: R$ ${troco},00</strong></p>`;
+  let troco = valorPago - valorCompra;
+  resultado += `<p><strong>Troco: R$ ${troco},00</strong></p>`;
 
-      for(let nota of notas){
-        
-      }
+  for (let nota of notas) {
+    let qtd = Math.floor(troco / nota);
+    troco = troco % nota;
+    resultado += `<p>Notas de R$ ${nota},00: ${qtd}</p>`;
+  }
+  return resultado;
+}
 
+function calcular() {
+  const compra = parseInt(document.getElementById("compra").value);
+  const pago = parseInt(document.getElementById("pago").value);
 
+  if (isNaN(compra) || isNaN(pago)) {
+    document.getElementById("saida").innerHTML = "Preencha os dois valores!";
+    return;
+  }
 
+  const saida = calcularTroco(compra, pago)
+  document.getElementById("saida").innerHTML = saida;
 }
